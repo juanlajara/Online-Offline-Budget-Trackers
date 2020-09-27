800e47f2afd71f79ba20370ccff990fb7a1d6c40
juanlajara <juanlajara001@gmail.com>
2020-09-27 18:27:04 -0500

    + dotenv for heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const environment = process.env.NODE_ENV || "development";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,14 +17,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+	useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+	console.log(`App running on port ${PORT}!`);
 });
